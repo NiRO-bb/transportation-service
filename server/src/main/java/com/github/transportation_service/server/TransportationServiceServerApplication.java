1bfc909b96b6f0f8fb3ac3a4fb01672198efdc03
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-02-26 11:36:13 +0400

    Create server directory

---
+++ server/src/main/java/com/github/transportation_service/server/TransportationServiceServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.transportation_service.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportationServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportationServiceServerApplication.class, args);
+	}
+
+}
