583e29c0f99dbb2766a0b3dc0d0b1ce7e7f81ece
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-03-02 12:29:39 +0400

    Add SignUp feature
    Added SignUpController to process HTTP-responses;
    Added SignUpRepository to connect database;

---
+++ server/src/main/java/com/github/transportation_service/server/controller/SignUpController.java
@@ -0,0 +1,43 @@
+package com.github.transportation_service.server.controller;
+
+import com.github.transportation_service.server.repository.SignUpRepository;
+import com.github.transportation_service.server.repository.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SignUpController {
+
+    @Autowired
+    SignUpRepository signUpRepository;
+
+    // зарегистрироваться
+    @PostMapping("/sign_up")
+    public boolean createAccount(@RequestBody User user) {
+
+        boolean result = false;
+
+        if (isCorrect(user)) {
+            signUpRepository.addUser(user);
+            result = true;
+        }
+
+        return result;
+    }
+
+    // проверить корректность данных
+    private boolean isCorrect(User user) {
+        boolean result = true;
+
+        // проверка
+        // 1. проверить логин на уникальность
+        if (signUpRepository.isUserExist(user.getLogin())) {
+            result = false;
+        }
+
+        System.out.println("isCorrect() method:\nuserLogin: '%s'\nresult: '%s'".formatted(user.getLogin(), result));
+        return result;
+    }
+}
